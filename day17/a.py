22768349e9604bb1ad41e2ca105b753829653cff
Guido Mainardi <guidoluis.gm@gmail.com>
2022-12-17 15:14:54 -0300

    day 17 ⭐⭐

--- day17/a.py
+++ day17/a.py
@@ -1,15 +1,13 @@
 from dataclasses import dataclass
-from tqdm import tqdm
 
-@dataclass(order=True)
+@dataclass(frozen=True, order=True)
 class Point:
     x: int
     y: int
 
 @dataclass()
 class Shape:
-    points: list[Point]
-
+    points: set[Point]
 
     def can_drop(self, floor):
         for p in self.points:
@@ -26,69 +24,60 @@ class Shape:
         return True
 
     def drop(self):
-        for p in self.points:
-            p.y += 1
+        self.points = set([Point(p.x, p.y+1) for p in self.points])
     
     def move(self, direction, floor):
         if not self.can_move(direction, floor):
             return
+        self.points = set([Point(p.x+direction, p.y) for p in self.points])
+
+    @property
+    def highest_point(self):
+        highest = 0
         for p in self.points:
-            p.x += direction
-        
-    def __str__(self) -> str:
-        s_x = sorted(self.points, key=lambda item: item.x)[0].x
-        s_y = sorted(self.points, key=lambda item: item.y)[0].y
-
-        e_x = sorted(self.points, key=lambda item: item.x)[-1].x
-        e_y = sorted(self.points, key=lambda item: item.y)[-1].y
-
-        ans = ''
-        for y in range(s_y, e_y+1):
-            for x in range(s_x, e_x+1):
-                if Point(x, y) in self.points:
-                    ans += '@'
-                else:
-                    ans += ' '
-            ans += '\n'
-        return ans
+            highest = min(p.y, highest)
+        return highest
 
 class Fblock(Shape):
 
-    def __init__(self) -> None:
+    def __init__(self, highest) -> None:
 
-        start_points = [Point(2, 0), Point(3, 0), Point(4, 0), Point(5, 0)]
-
+        start_points = set([Point(2, 0), Point(3, 0), Point(4, 0), Point(5, 0)])
+        start_points = set([Point(p.x, p.y + highest - 4) for p in start_points])
         super().__init__(start_points)
     
 class Pblock(Shape):
 
-    def __init__(self) -> None:
+    def __init__(self, highest) -> None:
 
-        start_points = [Point(3, -2), Point(2, -1), Point(3, -1), Point(4, -1), Point(3, 0)]
-
+        start_points = set([Point(3, -2), Point(2, -1), Point(3, -1), Point(4, -1), Point(3, 0)])
+        start_points = set([Point(p.x, p.y + highest - 4) for p in start_points])
         super().__init__(start_points)
     
 class Jblock(Shape):
 
-    def __init__(self) -> None:
+    def __init__(self, highest) -> None:
 
-        start_points = [Point(4, -2), Point(4, -1), Point(2, 0), Point(3, 0), Point(4, 0)]
+        start_points = set([Point(4, -2), Point(4, -1), Point(2, 0), Point(3, 0), Point(4, 0)])
+        start_points = set([Point(p.x, p.y + highest - 4) for p in start_points])
 
         super().__init__(start_points)
     
 class Iblock(Shape):
 
-    def __init__(self) -> None:
+    def __init__(self, highest) -> None:
 
-        start_points = [Point(2, -3), Point(2, -2), Point(2, -1), Point(2, 0)]
+        start_points = set([Point(2, -3), Point(2, -2), Point(2, -1), Point(2, 0)])
+        start_points = set([Point(p.x, p.y + highest - 4) for p in start_points])
 
         super().__init__(start_points)
     
 class Oblock(Shape):
 
-    def __init__(self) -> None:
+    def __init__(self, highest) -> None:
 
-        start_points = [Point(2, -1), Point(2, 0), Point(3, -1), Point(3, 0)]
+        start_points = set([Point(2, -1), Point(2, 0), Point(3, -1), Point(3, 0)])
+        start_points = set([Point(p.x, p.y + highest - 4) for p in start_points])
 
         super().__init__(start_points)
 
@@ -99,62 +88,40 @@ class Moves:
         self.id = 0
     
     def get_next(self):
-        self.id += 1
-        return self.moves[(self.id-1) % len(self.moves)]
+        next_move = self.moves[self.id]
+        self.id = (self.id+1) % len(self.moves)
+        return next_move
     
-def get_next_shape(round):
+def get_next_shape(round, highest):
     round %= 5
     match round:
         case 0:
-            return Fblock()
+            return Fblock(highest)
         case 1:
-            return Pblock()
+            return Pblock(highest)
         case 2:
-            return Jblock()
+            return Jblock(highest)
         case 3:
-            return Iblock()
+            return Iblock(highest)
         case 4:
-            return Oblock()
+            return Oblock(highest)
 
     return None
 
-def update_floor(floor, shape):
-    start = sorted(floor, key=lambda item: item.y)[0].y
-    floor = [*floor, *shape.points]
-    end = sorted(floor, key=lambda item: item.y)[0].y
-    for p in floor:
-        p.y += (start - end)
-    return (start - end), floor
-
-def print_result(floor):
-    s_x = sorted(floor, key=lambda item: item.x)[0].x
-    s_y = sorted(floor, key=lambda item: item.y)[0].y
-
-    e_x = sorted(floor, key=lambda item: item.x)[-1].x
-    e_y = sorted(floor, key=lambda item: item.y)[-1].y
-
-    ans = ''
-    for y in range(s_y, e_y+1):
-        for x in range(s_x, e_x+1):
-            if Point(x, y) in floor:
-                ans += '#'
-            else:
-                ans += '.'
-        ans += '\n'
-    print(ans)
-
 moves = Moves('input.txt')
-floor = [Point(0, 4), Point(1, 4), Point(2, 4), Point(3, 4), Point(4, 4), Point(5, 4), Point(6, 4)]
+floor = set([Point(0, 0), Point(1, 0), Point(2, 0), Point(3, 0), Point(4, 0), Point(5, 0), Point(6, 0)])
+highest = 0
 total = 0
-for round in tqdm(range(2022)):
-    shape = get_next_shape(round)
+for rock in range(2022):
+    shape = get_next_shape(rock, highest)
 
     shape.move(moves.get_next(), floor)
+
     while shape.can_drop(floor):
         shape.drop()
         shape.move(moves.get_next(), floor)
 
-    down, floor = update_floor(floor, shape)
-    total += down
-print(total)
-print('\n')
+    floor = floor.union(shape.points)
+    highest = min(highest, shape.highest_point)
+    
+print(abs(highest))
--- day17/b.py
+++ day17/b.py
@@ -1,15 +1,14 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from tqdm import tqdm
 
-@dataclass(order=True)
+@dataclass(frozen=True, order=True)
 class Point:
     x: int
     y: int
 
 @dataclass()
 class Shape:
-    points: list[Point]
-
+    points: set[Point]
 
     def can_drop(self, floor):
         for p in self.points:
@@ -26,69 +25,60 @@ class Shape:
         return True
 
     def drop(self):
-        for p in self.points:
-            p.y += 1
+        self.points = set([Point(p.x, p.y+1) for p in self.points])
     
     def move(self, direction, floor):
         if not self.can_move(direction, floor):
             return
+        self.points = set([Point(p.x+direction, p.y) for p in self.points])
+
+    @property
+    def highest_point(self):
+        highest = 0
         for p in self.points:
-            p.x += direction
-        
-    def __str__(self) -> str:
-        s_x = sorted(self.points, key=lambda item: item.x)[0].x
-        s_y = sorted(self.points, key=lambda item: item.y)[0].y
-
-        e_x = sorted(self.points, key=lambda item: item.x)[-1].x
-        e_y = sorted(self.points, key=lambda item: item.y)[-1].y
-
-        ans = ''
-        for y in range(s_y, e_y+1):
-            for x in range(s_x, e_x+1):
-                if Point(x, y) in self.points:
-                    ans += '@'
-                else:
-                    ans += ' '
-            ans += '\n'
-        return ans
+            highest = min(p.y, highest)
+        return highest
 
 class Fblock(Shape):
 
-    def __init__(self) -> None:
+    def __init__(self, highest) -> None:
 
-        start_points = [Point(2, 0), Point(3, 0), Point(4, 0), Point(5, 0)]
-
+        start_points = set([Point(2, 0), Point(3, 0), Point(4, 0), Point(5, 0)])
+        start_points = set([Point(p.x, p.y + highest - 4) for p in start_points])
         super().__init__(start_points)
     
 class Pblock(Shape):
 
-    def __init__(self) -> None:
+    def __init__(self, highest) -> None:
 
-        start_points = [Point(3, -2), Point(2, -1), Point(3, -1), Point(4, -1), Point(3, 0)]
-
+        start_points = set([Point(3, -2), Point(2, -1), Point(3, -1), Point(4, -1), Point(3, 0)])
+        start_points = set([Point(p.x, p.y + highest - 4) for p in start_points])
         super().__init__(start_points)
     
 class Jblock(Shape):
 
-    def __init__(self) -> None:
+    def __init__(self, highest) -> None:
 
-        start_points = [Point(4, -2), Point(4, -1), Point(2, 0), Point(3, 0), Point(4, 0)]
+        start_points = set([Point(4, -2), Point(4, -1), Point(2, 0), Point(3, 0), Point(4, 0)])
+        start_points = set([Point(p.x, p.y + highest - 4) for p in start_points])
 
         super().__init__(start_points)
     
 class Iblock(Shape):
 
-    def __init__(self) -> None:
+    def __init__(self, highest) -> None:
 
-        start_points = [Point(2, -3), Point(2, -2), Point(2, -1), Point(2, 0)]
+        start_points = set([Point(2, -3), Point(2, -2), Point(2, -1), Point(2, 0)])
+        start_points = set([Point(p.x, p.y + highest - 4) for p in start_points])
 
         super().__init__(start_points)
     
 class Oblock(Shape):
 
-    def __init__(self) -> None:
+    def __init__(self, highest) -> None:
 
-        start_points = [Point(2, -1), Point(2, 0), Point(3, -1), Point(3, 0)]
+        start_points = set([Point(2, -1), Point(2, 0), Point(3, -1), Point(3, 0)])
+        start_points = set([Point(p.x, p.y + highest - 4) for p in start_points])
 
         super().__init__(start_points)
 
@@ -99,62 +89,58 @@ class Moves:
         self.id = 0
     
     def get_next(self):
-        self.id += 1
-        return self.moves[(self.id-1) % len(self.moves)]
+        next_move = self.moves[self.id]
+        self.id = (self.id+1) % len(self.moves)
+        return next_move
     
-def get_next_shape(round):
+def get_next_shape(round, highest):
     round %= 5
     match round:
         case 0:
-            return Fblock()
+            return Fblock(highest)
         case 1:
-            return Pblock()
+            return Pblock(highest)
         case 2:
-            return Jblock()
+            return Jblock(highest)
         case 3:
-            return Iblock()
+            return Iblock(highest)
         case 4:
-            return Oblock()
+            return Oblock(highest)
 
     return None
 
-def update_floor(floor, shape):
-    start = sorted(floor, key=lambda item: item.y)[0].y
-    floor = [*floor, *shape.points]
-    end = sorted(floor, key=lambda item: item.y)[0].y
-    for p in floor:
-        p.y += (start - end)
-    return (start - end), floor
-
-def print_result(floor):
-    s_x = sorted(floor, key=lambda item: item.x)[0].x
-    s_y = sorted(floor, key=lambda item: item.y)[0].y
-
-    e_x = sorted(floor, key=lambda item: item.x)[-1].x
-    e_y = sorted(floor, key=lambda item: item.y)[-1].y
-
-    ans = ''
-    for y in range(s_y, e_y+1):
-        for x in range(s_x, e_x+1):
-            if Point(x, y) in floor:
-                ans += '#'
-            else:
-                ans += '.'
-        ans += '\n'
-    print(ans)
-
 moves = Moves('input.txt')
-floor = [Point(0, 4), Point(1, 4), Point(2, 4), Point(3, 4), Point(4, 4), Point(5, 4), Point(6, 4)]
-total = 0
-for round in tqdm(range(2022)):
-    shape = get_next_shape(round)
+floor = set([Point(0, 0), Point(1, 0), Point(2, 0), Point(3, 0), Point(4, 0), Point(5, 0), Point(6, 0)])
+
+highest = 0
+rock = 0
+rock_to_drop = 1_000_000_000_000
+cache = {}
+while rock < rock_to_drop:
+
+    seen = (rock%5, moves.id)
+    if seen in cache:
+        period = rock - cache[seen][0]
+        if rock % period == rock_to_drop % period:
+            stored_h = cache[seen][1]
+            period_h = abs(highest) - stored_h
+            period_ocr = ((rock_to_drop-rock)//period) + 1
+            print(stored_h + period_h * period_ocr)
+            break
+    else:
+        cache[seen] = rock, abs(highest)
+    
+    shape = get_next_shape(rock, highest)
 
     shape.move(moves.get_next(), floor)
+
     while shape.can_drop(floor):
         shape.drop()
         shape.move(moves.get_next(), floor)
 
-    down, floor = update_floor(floor, shape)
-    total += down
-print(total)
-print('\n')
+
+    floor = floor.union(shape.points)
+       
+    highest = min(highest, shape.highest_point)
+
+    rock += 1
